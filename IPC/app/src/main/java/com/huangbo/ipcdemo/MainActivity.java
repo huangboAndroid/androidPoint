85dce68117917782f03b1e946f2128f59b5dedbf
huangboAndroid <so_huangbo@163.com>
2018-09-18 22:31:30 +0800

    Messenger跨进程通信

--- IPC/app/src/main/java/com/huangbo/ipcdemo/MainActivity.java
+++ IPC/app/src/main/java/com/huangbo/ipcdemo/MainActivity.java
@@ -1,16 +1,60 @@
 package com.huangbo.ipcdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.os.RemoteException;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.huangbo.ipcdemo.service.MessagerService;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Messenger mMessenger;
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            mMessenger = new Messenger(iBinder);
+            Message msg = Message.obtain(null, 0);
+            Bundle bundle = new Bundle();
+            bundle.putString("msg", "hello, I am Client");
+            msg.setData(bundle);
+            try {
+                mMessenger.send(msg);
+            } catch (RemoteException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //测试
 
+    }
 
+    public void bindService(View v) {
+        Intent intent = new Intent(this, MessagerService.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() {
+        unbindService(mConnection);
+        super.onDestroy();
     }
 }
--- IPC/app/src/main/java/com/huangbo/ipcdemo/service/MessagerService.java
+++ IPC/app/src/main/java/com/huangbo/ipcdemo/service/MessagerService.java
@@ -1,10 +1,41 @@
 package com.huangbo.ipcdemo.service;
 
 import android.app.Service;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.support.annotation.Nullable;
+import android.util.Log;
 
 
 /**
  * created by huangbo at 2018/9/18 21:09
  */
 public class MessagerService extends Service {
+    private static final String TAG = "MessagerService";
+
+    private final Messenger mMessenger = new Messenger(new MessengerHandler());
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return mMessenger.getBinder();
+    }
+
+
+
+    private static class MessengerHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case 0:
+                    Log.e(TAG, "receive msg from Client: "+ msg.getData().getString("msg"));
+                    break;
+                    default:
+                        super.handleMessage(msg);
+            }
+
+        }
+    }
 }
