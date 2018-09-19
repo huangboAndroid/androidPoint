904ab24356ab9e4bad1ffa956f2da575278bd418
huangbo <huangbo@chetong.net>
2018-09-19 09:28:02 +0800

    test

--- IPC/app/src/main/java/com/huangbo/ipcdemo/service/MessagerService.java
+++ IPC/app/src/main/java/com/huangbo/ipcdemo/service/MessagerService.java
@@ -38,4 +38,28 @@ public class MessagerService extends Service {
 
         }
     }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.e(TAG, "onCreate");
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.e(TAG, "onStartCommand");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onRebind(Intent intent) {
+        Log.e(TAG, "onRebind");
+        super.onRebind(intent);
+    }
+
+    @Override
+    public boolean onUnbind(Intent intent) {
+        Log.e(TAG, "onUnbind");
+        return super.onUnbind(intent);
+    }
 }
